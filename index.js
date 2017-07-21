bbe8f3c568895aa7640061918838c68715f8d4c1
Daniel Kovalevich <dkoval526@gmail.com>
2017-07-21 12:53:07 -0400

    It is working well now

--- index.js
+++ index.js
@@ -32,20 +32,89 @@ let handlers = {
         else if (!statistic) {
             // format stats so it shows only the essential info
             mma.fighter(fighter, function (data) {
-                let speechOutput = data.fullname + " is a " + data.weight_class + " from " + data.nationality + " with a record of " + data.wins.total + " wins and " + data.losses.total + " losses";
+                let speechOutput = data.fullname + ' is a ' + data.weight_class + ' from ' + data.nationality + ' with a record of ' + data.wins.total + ' wins and ' + data.losses.total + ' losses';
                 Object.emit(':tell', speechOutput);
             });
         }
         else {
             // format stats so it shows only the essential info
             mma.fighter(fighter, function (data) {
-                if (!data[statistic]) {
-                    Object.emit(':tell', "Sorry. I don't think I know that one.");
-                }
-                else {
-                    
+                // some of these need to be fixed so it matches json data
+                if (statistic == "weight class")
+                    statistic = 'weight_class';
+                if (statistic == 'last fight')
+                    statistic = 'fights';
+                if (statistic == 'weight in kilograms')
+                    statistic = 'weight_kg';
+                if (statistic == 'height in centimeters')
+                    statistic = 'height_cm';
+                if (statistic == 'old') 
+                    statistic = 'age';
 
-                    let speechOutput = data.name + "'s " + statistic + " is " + data[statistic];
+                if (!data[statistic]) {
+                    Object.emit(':tell', 'Sorry. I don\'t think I know that one.');
+                }
+
+                // some of the data are objects within the json so I need to break it up
+                else {
+                    let name = data.name;
+                    let speechOutput = name;
+                    data = data[statistic];
+                    switch (statistic) {
+                        case 'wins':
+                        case 'losses':
+                            speechOutput += ' has a total of '
+                                + data.total + ' ' + statistic + '. The ' + statistic + ' are by '
+                                + data.knockouts + " knockouts, "
+                                + data.submissions + " submissions, "
+                                + data.decisions + " decisions, and "
+                                + data.others + " other " + statistic.
+                            break;
+                        case 'strikes':
+                            speechOutput += ' has attempted '
+                                + data.attempted + ' strikes; '
+                                + data.successful + ' were successful. '
+                                + data.standing + ' were while standing, '
+                                + data.clinch + ' were in the clinch, and '
+                                + data.ground + ' were on the ground.';
+                            break;
+                        case 'takedowns':
+                            speechOutput += ' has attempted '
+                                + data.attempted + ' takedowns; '
+                                + data.successful + ' were successful. '
+                                + data.submissions + ' were submissions, '
+                                + data.passes + ' were passes, and '
+                                + data.sweeps + ' were sweeps.';
+                            break;
+                        case 'fights':
+                            data = data[0];
+                            speechOutput += '\'s last fight was ' 
+                                + data.name + '. It happened on '
+                                + data.date + '. ' + name + ' fought '
+                                + data.opponent + '. It ended as a ' 
+                                + ((data.result == "NC") ? "no contest" : data.result) + ' by ' 
+                                + ((data.method == "NC") ? "no contest" : data.result) + '. The fight went ' 
+                                + data.round + ' rounds and was refereed by '
+                                + data.referee;
+
+                            break;
+                        case 'weight_class':
+                            speechOutput += "'s weight class is, " + data;
+                            break;
+                        case 'weight_kg':
+                            speechOutput += "'s weight is " + data + ' kilograms';
+                            break;
+                        case 'height_cm':
+                            speechOutput += "'s height is " + data + ' centimeters';
+                            break;
+                        case 'age':
+                            speechOutput += ' is ' + data + ' years old';
+                            break;
+                        default:
+                            speechOutput += "'s " + statistic + " is, " + data;
+                            break;
+                    }
+
                     Object.emit(':tell', speechOutput);
                 }
             });
@@ -54,7 +123,7 @@ let handlers = {
     'AMAZON.HelpIntent': function () {
         var speechOutput = HELP_MESSAGE;
         var repromt = HELP_REPROMPT;
-        this.emit(':ask', speechOutput.repromt);
+        this.emit(':tell', speechOutput);
     },
     'AMAZON.CancelIntent': function () {
         this.emit(':tell', STOP_MESSAGE);
@@ -65,8 +134,4 @@ let handlers = {
     'Unhandled': function () {
         this.emit(':ask', HELP_MESSAGE, HELP_MESSAGE);
     }
-}
-
-function fighterStatistics() {
-
 }
--- src/test.js
+++ src/test.js
@@ -2,26 +2,37 @@ var mma = require('mma');
 
 mma.fighter("Jon Jones", function (data) {
     let fighter = "jones";
-    let statistic = 'age';
+    let statistic = 'wins';
     if (!fighter) {
         let speechOutput = "HELP_REPROMPT";
         console.log(speechOutput);
     }
     else if (!statistic) {
         // format stats so it shows only the essential info
-        mma.fighter(fighter, function(data) {
+        mma.fighter(fighter, function (data) {
             let speechOutput = data.fullname + " is a " + data.weight_class + " from " + data.nationality + " with a record of " + data.wins.total + " wins and " + data.losses.total + " losses";
             console.log(speechOutput);
         });
     }
     else {
         // format stats so it shows only the essential info
-        mma.fighter(fighter, function(data) {
+        mma.fighter(fighter, function (data) {
             if (!data[statistic]) {
                 console.log("Sorry. I don't think I know that one.");
             }
             else {
-                let speechOutput = data.name + "'s " + statistic + " is " + data[statistic];
+                let speechOutput = data.name;
+
+                switch (statistic) {
+                    case 'wins':
+                        speechOutput += ' has a total of ' + data[statistic].total + ' wins. '
+                            + data[statistic].knockouts + " knockouts, " + data[statistic].submissions + " submissions, "
+                            + data[statistic].decisions + " decisions, and " + data[statistic].others + " other wins."
+                        break;
+                    default:
+                        speechOutput += "'s " + statistic + " is, " + data[statistic];
+                        break;
+                }
                 console.log(speechOutput);
             }
         });
