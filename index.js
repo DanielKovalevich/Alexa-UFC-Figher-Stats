64538d5c942b9a843dc454efd1bd34541dced155
Daniel Kovalevich <dkoval526@gmail.com>
2017-07-22 11:22:21 -0400

    Fixed all previous issues

--- index.js
+++ index.js
@@ -16,114 +16,124 @@ exports.handler = function (event, context, callback) {
 };
 
 let handlers = {
-    'LaunchRequest': function () {
-        this.emit(':tell', HELP_REPROMPT);
-    },
     'fighterStats': function () {
         var Object = this;
         var intentObj = this.event.request.intent;
         let fighter = intentObj.slots.fighter.value;
         let statistic = intentObj.slots.statistic.value;
         if (!fighter) {
-            let slotToElicit = 'AMAZON.US_FIRST_NAME';
-            let speechOutput = HELP_REPROMPT;
-            this.emit(':elicitSlot', slotToElicit, speechOutput, speechOutput);
+            let slotToElicit = 'AMAZON.Athlete';
+            let speechOutput = "That doesn't seem to be a real fighter. Try another.";
+            Object.emit(':elicitSlot', slotToElicit, speechOutput, HELP_REPROMPT);
+            let fighter = intentObj.slots.fighter.value;
         }
         else if (!statistic) {
             // format stats so it shows only the essential info
             mma.fighter(fighter, function (data) {
-                let speechOutput = data.fullname + ' is a ' + data.weight_class + ' from ' + data.nationality + ' with a record of ' + data.wins.total + ' wins and ' + data.losses.total + ' losses';
-                Object.emit(':tell', speechOutput);
+                if (data == null) {
+                    let slotToElicit = 'AMAZON.Athlete';
+                    let speechOutput = HELP_REPROMPT;
+                    Object.emit(':elicitSlot', slotToElicit, speechOutput, speechOutput);
+                    let fighter = intentObj.slots.fighter.value;
+                }
+                else {
+                    let speechOutput = data.fullname + ' is a ' + data.weight_class + ' from ' + data.nationality + ' with a record of ' + data.wins.total + ' wins and ' + data.losses.total + ' losses';
+                    Object.emit(':tell', speechOutput);
+                }
             });
         }
         else {
             // format stats so it shows only the essential info
             mma.fighter(fighter, function (data) {
-                // some of these need to be fixed so it matches json data
-                if (statistic == "weight class")
-                    statistic = 'weight_class';
-                if (statistic == 'last fight')
-                    statistic = 'fights';
-                if (statistic == 'weight in kilograms')
-                    statistic = 'weight_kg';
-                if (statistic == 'height in centimeters')
-                    statistic = 'height_cm';
-                if (statistic == 'old') 
-                    statistic = 'age';
-
-                if (!data[statistic]) {
-                    Object.emit(':tell', 'Sorry. I don\'t think I know that one.');
+                if (data == null) {
+                    Object.emit(':tell', "That is not a real fighter!");
                 }
-
-                // some of the data are objects within the json so I need to break it up
                 else {
-                    let name = data.name;
-                    let speechOutput = name;
-                    data = data[statistic];
-                    switch (statistic) {
-                        case 'wins':
-                        case 'losses':
-                            speechOutput += ' has a total of '
-                                + data.total + ' ' + statistic + '. The ' + statistic + ' are by '
-                                + data.knockouts + " knockouts, "
-                                + data.submissions + " submissions, "
-                                + data.decisions + " decisions, and "
-                                + data.others + " other " + statistic.
-                            break;
-                        case 'strikes':
-                            speechOutput += ' has attempted '
-                                + data.attempted + ' strikes; '
-                                + data.successful + ' were successful. '
-                                + data.standing + ' were while standing, '
-                                + data.clinch + ' were in the clinch, and '
-                                + data.ground + ' were on the ground.';
-                            break;
-                        case 'takedowns':
-                            speechOutput += ' has attempted '
-                                + data.attempted + ' takedowns; '
-                                + data.successful + ' were successful. '
-                                + data.submissions + ' were submissions, '
-                                + data.passes + ' were passes, and '
-                                + data.sweeps + ' were sweeps.';
-                            break;
-                        case 'fights':
-                            data = data[0];
-                            speechOutput += '\'s last fight was ' 
-                                + data.name + '. It happened on '
-                                + data.date + '. ' + name + ' fought '
-                                + data.opponent + '. It ended as a ' 
-                                + ((data.result == "NC") ? "no contest" : data.result) + ' by ' 
-                                + ((data.method == "NC") ? "no contest" : data.result) + '. The fight went ' 
-                                + data.round + ' rounds and was refereed by '
-                                + data.referee;
+                    // some of these need to be fixed so it matches json data
+                    if (statistic == "weight class")
+                        statistic = 'weight_class';
+                    if (statistic == 'last fight')
+                        statistic = 'fights';
+                    if (statistic == 'weight in kilograms')
+                        statistic = 'weight_kg';
+                    if (statistic == 'height in centimeters')
+                        statistic = 'height_cm';
+                    if (statistic == 'old')
+                        statistic = 'age';
 
-                            break;
-                        case 'weight_class':
-                            speechOutput += "'s weight class is, " + data;
-                            break;
-                        case 'weight_kg':
-                            speechOutput += "'s weight is " + data + ' kilograms';
-                            break;
-                        case 'height_cm':
-                            speechOutput += "'s height is " + data + ' centimeters';
-                            break;
-                        case 'age':
-                            speechOutput += ' is ' + data + ' years old';
-                            break;
-                        default:
-                            speechOutput += "'s " + statistic + " is, " + data;
-                            break;
+                    if (!data[statistic]) {
+                        let slotToElicit = 'statistic';
+                        let speechOutput = 'That doesn\'t seem to be an available statistic. What statistic do you want to know about?';
+                        let speechRepromt = 'Still not quite. Try another statistic.';
+                        Object.emit(':elicitSlot', slotToElicit, speechOutput, speechRepromt);
+                        let statistic = intentObj.slots.statistic.value;
                     }
 
-                    Object.emit(':tell', speechOutput);
+                    // some of the data are objects within the json so I need to break it up
+                    else {
+                        let name = data.name;
+                        let speechOutput = name;
+                        data = data[statistic];
+                        switch (statistic) {
+                            case 'wins':
+                            case 'losses':
+                                speechOutput += ' has a total of '
+                                    + data.total + ' ' + statistic + '. The ' + statistic + ' are by '
+                                    + data.knockouts + " knockouts, "
+                                    + data.submissions + " submissions, "
+                                    + data.decisions + " decisions, and "
+                                    + data.others + " other " + statistic + '.';
+                                break;
+                            case 'strikes':
+                                speechOutput += ' has attempted '
+                                    + data.attempted + ' strikes; '
+                                    + data.successful + ' were successful. '
+                                    + data.standing + ' were while standing, '
+                                    + data.clinch + ' were in the clinch, and '
+                                    + data.ground + ' were on the ground.';
+                                break;
+                            case 'takedowns':
+                                speechOutput += ' has attempted '
+                                    + data.attempted + ' takedowns; '
+                                    + data.successful + ' were successful. '
+                                    + data.submissions + ' were submissions, '
+                                    + data.passes + ' were passes, and '
+                                    + data.sweeps + ' were sweeps.';
+                                break;
+                            case 'fights':
+                                data = data[0];
+                                speechOutput += '\'s last fight was '
+                                    + data.name + '. It happened on '
+                                    + data.date + '. ' + name + ' fought '
+                                    + data.opponent + '. It ended as a '
+                                    + ((data.result == "NC") ? "no contest" : data.result) + ' by '
+                                    + ((data.method == "NC") ? "no contest" : data.result) + '. The fight went '
+                                    + data.round + ' rounds and was refereed by '
+                                    + data.referee;
+
+                                break;
+                            case 'weight_class':
+                                speechOutput += "'s weight class is, " + data;
+                                break;
+                            case 'weight_kg':
+                                speechOutput += "'s weight is " + data + ' kilograms';
+                                break;
+                            case 'height_cm':
+                                speechOutput += "'s height is " + data + ' centimeters';
+                                break;
+                            case 'age':
+                                speechOutput += ' is ' + data + ' years old';
+                                break;
+                            default:
+                                speechOutput += "'s " + statistic + " is, " + data;
+                                break;
+                        }
+
+                        Object.emit(':tell', speechOutput);
+                    }
                 }
             });
         }
-    },
-    'AMAZON.HelpIntent': function () {
-        var speechOutput = HELP_MESSAGE;
-        var repromt = HELP_REPROMPT;
-        this.emit(':tell', speechOutput);
     },
     'AMAZON.CancelIntent': function () {
         this.emit(':tell', STOP_MESSAGE);
@@ -131,7 +141,16 @@ let handlers = {
     'AMAZON.StopIntent': function () {
         this.emit(':tell', STOP_MESSAGE);
     },
+    'SessionEndedRequest': function () {
+        this.emit(':tell', STOP_MESSAGE);
+    },
     'Unhandled': function () {
-        this.emit(':ask', HELP_MESSAGE, HELP_MESSAGE);
-    }
-}
+        this.emit(':ask', HELP_MESSAGE, HELP_REPROMPT);
+    },
+    'AMAZON.HelpIntent': function () {
+        this.emit(':ask', HELP_MESSAGE, HELP_REPROMPT);
+    },
+    'LaunchRequest': function () {
+        this.emit(':ask', HELP_MESSAGE, HELP_REPROMPT);
+    },
+};
--- src/test.js
+++ src/test.js
@@ -1,40 +1,112 @@
 var mma = require('mma');
 
-mma.fighter("Jon Jones", function (data) {
-    let fighter = "jones";
-    let statistic = 'wins';
-    if (!fighter) {
-        let speechOutput = "HELP_REPROMPT";
-        console.log(speechOutput);
-    }
-    else if (!statistic) {
-        // format stats so it shows only the essential info
-        mma.fighter(fighter, function (data) {
-            let speechOutput = data.fullname + " is a " + data.weight_class + " from " + data.nationality + " with a record of " + data.wins.total + " wins and " + data.losses.total + " losses";
-            console.log(speechOutput);
-        });
+let fighter = 'pizza';
+let statistic = 'wins';
+
+mma.fighter(fighter, function (data) {
+    if (data == null) {
+        console.log('it ends here');
     }
     else {
-        // format stats so it shows only the essential info
-        mma.fighter(fighter, function (data) {
-            if (!data[statistic]) {
-                console.log("Sorry. I don't think I know that one.");
-            }
-            else {
-                let speechOutput = data.name;
-
-                switch (statistic) {
-                    case 'wins':
-                        speechOutput += ' has a total of ' + data[statistic].total + ' wins. '
-                            + data[statistic].knockouts + " knockouts, " + data[statistic].submissions + " submissions, "
-                            + data[statistic].decisions + " decisions, and " + data[statistic].others + " other wins."
-                        break;
-                    default:
-                        speechOutput += "'s " + statistic + " is, " + data[statistic];
-                        break;
+        console.log(data);
+        if (!fighter) {
+            let speechOutput = "HELP_REPROMPT";
+            console.log(speechOutput);
+        }
+        else if (!statistic) {
+            // format stats so it shows only the essential info
+            mma.fighter(fighter, function (data) {
+                if (data == null) {
+                    console.log('it ends here');
                 }
+                let speechOutput = data.fullname + " is a " + data.weight_class + " from " + data.nationality + " with a record of " + data.wins.total + " wins and " + data.losses.total + " losses";
                 console.log(speechOutput);
-            }
-        });
+            });
+        }
+        else {
+            // format stats so it shows only the essential info
+            mma.fighter(fighter, function (data) {
+                if (data == null) {
+                    console.log('it ends here');
+                }
+                else {
+                    if (statistic == "weight class")
+                        statistic = 'weight_class';
+                    if (statistic == 'last fight')
+                        statistic = 'fights';
+                    if (statistic == 'weight in kilograms')
+                        statistic = 'weight_kg';
+                    if (statistic == 'height in centimeters')
+                        statistic = 'height_cm';
+                    if (statistic == 'old')
+                        statistic = 'age';
+
+                    if (!data[statistic]) {
+                        console.log("Sorry. I don't think I know that one.");
+                    }
+                    else {
+                        let name = data.name;
+                        let speechOutput = name;
+                        data = data[statistic];
+                        console.log(data);
+                        switch (statistic) {
+                            case 'wins':
+                            case 'losses':
+                                speechOutput += ' has a total of '
+                                    + data.total + ' ' + statistic + '. The ' + statistic + ' are by '
+                                    + data.knockouts + " knockouts, "
+                                    + data.submissions + " submissions, "
+                                    + data.decisions + " decisions, and "
+                                    + data.others + " other " + statistic + '.';
+                                break;
+                            case 'strikes':
+                                speechOutput += ' has attempted '
+                                    + data.attempted + ' strikes; '
+                                    + data.successful + ' were successful. '
+                                    + data.standing + ' were while standing, '
+                                    + data.clinch + ' were in the clinch, and '
+                                    + data.ground + ' were on the ground.';
+                                break;
+                            case 'takedowns':
+                                speechOutput += ' has attempted '
+                                    + data.attempted + ' takedowns; '
+                                    + data.successful + ' were successful. '
+                                    + data.submissions + ' were submissions, '
+                                    + data.passes + ' were passes, and '
+                                    + data.sweeps + ' were sweeps.';
+                                break;
+                            case 'fights':
+                                data = data[0];
+                                speechOutput += '\'s last fight was '
+                                    + data.name + '. It happened on '
+                                    + data.date + '. ' + name + ' fought '
+                                    + data.opponent + '. It ended as a '
+                                    + ((data.result == "NC") ? "no contest" : data.result) + ' by '
+                                    + ((data.method == "NC") ? "no contest" : data.result) + '. The fight went '
+                                    + data.round + ' rounds and was refereed by '
+                                    + data.referee;
+
+                                break;
+                            case 'weight_class':
+                                speechOutput += "'s weight class is, " + data;
+                                break;
+                            case 'weight_kg':
+                                speechOutput += "'s weight is " + data + ' kilograms';
+                                break;
+                            case 'height_cm':
+                                speechOutput += "'s height is " + data + ' centimeters';
+                                break;
+                            case 'age':
+                                speechOutput += ' is ' + data + ' years old';
+                                break;
+                            default:
+                                speechOutput += "'s " + statistic + " is, " + data;
+                                break;
+                        }
+                        console.log(speechOutput);
+                    }
+                }
+            });
+        }
     }
 });
